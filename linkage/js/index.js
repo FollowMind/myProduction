a8c6e03c120352f29cad97d823f4095df802cf25
FollowMind <792723986@qq.com>
2017-12-27 14:57:47 +0800

    三级联动

---
+++ linkage/js/index.js
@@ -0,0 +1,52 @@
+$(function(){
+	var capitalHtml = '';
+	var capitalLen = cities.data.length;
+	
+	initCapital();
+	function initCapital(){
+		for(var i = 0;i < capitalLen;i ++){
+			capitalHtml = '<option value="'+cities.data[i].name+'">'+cities.data[i].name+'</option>';
+			$('.capital').append(capitalHtml);
+		}
+	}
+	
+	var capitalVal = '';
+	var capitalIndex = 0;
+	var cityLen = 0;
+	var cityHtml = '';
+	
+	initCity();
+	function initCity(){
+		capitalVal = $('.capital option:selected').text();/*省份*/
+		capitalIndex = $('.capital  option:selected').index();/*省份索引*/
+		cityLen = cities.data[capitalIndex].city.length;/*市的长度*/
+		$('.cities').html('');
+		for(var i = 0;i < cityLen;i ++){
+			cityHtml = '<option>'+cities.data[capitalIndex].city[i].name+'</option>';
+			$('.cities').append(cityHtml);
+		}
+	}
+	var cityVal = '';
+	var cityIndex = 0;
+	var cityLen = 0;
+	var countyHtml = '';
+	
+	initCounty();
+	function initCounty(){
+		cityVal = $('.cities option:selected').text();/*城市*/
+		cityIndex = $('.cities option:selected').index();/*城市索引*/
+		cityLen = cities.data[capitalIndex].city[cityIndex].area.length;/*区县的长度*/
+		$('.county').html('');/*清空*/
+		for(var i = 0;i < cityLen;i ++){
+			countyHtml = '<option>'+cities.data[capitalIndex].city[cityIndex].area[i]+'</option>';
+			$('.county').append(countyHtml);
+		}	
+	}
+	$('.capital').change(function(){
+		initCity();
+		initCounty();
+	})
+	$('.cities').change(function(){
+		initCounty()
+	})
+})	
